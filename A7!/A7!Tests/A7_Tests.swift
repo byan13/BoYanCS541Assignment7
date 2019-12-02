c091522637ad936855347043be3f699a476514b9
Bo Yan <byan2@binghamton.edu>
2019-12-02 11:11:05 -0500

    Project added

---
+++ A7!/A7!Tests/A7_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  A7_Tests.swift
+//  A7!Tests
+//
+//  Created by Bo Yan on 12/2/19.
+//  Copyright © 2019 Bo Yan. All rights reserved.
+//
+
+import XCTest
+@testable import A7_
+
+class A7_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
